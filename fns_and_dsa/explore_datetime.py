26d179d70669b8a0a8aeea30d59a76e36aa7c84c
Alaa Okasha <mxmmotnt1@gmail.com>
2025-07-23 17:24:09 +0300

    Fixed microseconds error

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,14 +2,15 @@ from datetime import datetime, timedelta, time
 
 def display_current_datetime():
     current_date = datetime.now()
+    print("Current date and time:",current_date.strftime('%Y-%m-%d %H:%M:%S'))
     return current_date
 
 def calculate_future_date(number_of_days, current_date):
     added_days = timedelta(days=number_of_days)
     future_date = current_date+added_days
-    print("Future date:",future_date)
+    print("Future date:",future_date.strftime('%Y-%m-%d %H:%M:%S'))
 
-print("Current date and time:",display_current_datetime())
+curr = display_current_datetime()
 add = int(input("Enter the number of days to add to the current date: "))
-calculate_future_date(add, display_current_datetime())
+calculate_future_date(add, curr)
 
