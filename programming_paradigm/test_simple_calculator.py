7750a9b2bbb1c0233955a9383b7c9f577bcb1924
Alaa Okasha <mxmmotnt1@gmail.com>
2025-08-07 20:37:26 +0300

    Added unittesting for simple_calculator.py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,25 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(2, 0), 2)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+        self.assertEqual(self.calc.subtract(5, 2), 3)
+        self.assertEqual(self.calc.subtract(0, 4), -4)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(3, 5), 15)
+        self.assertEqual(self.calc.multiply(3, 0), 0)
+        self.assertEqual(self.calc.multiply(-20, 5), -100)
+    def test_dividision(self):
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+        self.assertIsNone(self.calc.divide(5, 0))
