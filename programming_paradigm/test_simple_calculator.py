9aa16b906ddbd1037caa0ee3e7db1429b52d89e3
Alaa Okasha <mxmmotnt1@gmail.com>
2025-08-07 20:38:10 +0300

    Fixed some typos in test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -19,7 +19,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(3, 5), 15)
         self.assertEqual(self.calc.multiply(3, 0), 0)
         self.assertEqual(self.calc.multiply(-20, 5), -100)
-    def test_dividision(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(0, 5), 0)
         self.assertIsNone(self.calc.divide(5, 0))
